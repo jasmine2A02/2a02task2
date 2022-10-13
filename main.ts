820af4b830e85e35ff4c84978225f13f358e22ee
jasmine2A02 <115674542+jasmine2A02@users.noreply.github.com>
2022-10-13 09:26:19 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . . # . .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . . # . .
+            . . # . .
+            . # . # .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
